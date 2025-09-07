58510f78b3085a78ce7104db3e4a01ee82d4dbb3
unknown <adilwibow@gmail.com>
2025-09-07 19:51:10 +0700

    data diperbarui

---
+++ home.php
@@ -0,0 +1,73 @@
+<nav class="navbar navbar-expand-lg bg-primary ">
+    <div class="container">
+        <a class="navbar-brand font-weight-bolder" href="index.php">
+            <h5 style="color:white;">Aplikasi Pengaduan Masyarakat</h5>
+        </a>
+        <div class="navbar-toggler" id="navbar" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+            <ul class="navbar-nav ms-md-auto justify-content-end">
+                <div class="sidenav-toggler-inner">
+                    <i class="sidenav-toggler-line bg-white"></i>
+                    <i class="sidenav-toggler-line bg-white"></i>
+                    <i class="sidenav-toggler-line bg-white"></i>
+                </div>
+            </ul>
+        </div>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?page=registrasi" style="color: white;">Daftar Akun</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?page=login" style="color: white;">Login</a>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+
+    <h4 class="text-center mt-3">Uji Kompetensi Keahlian <br> Rekayasa Perangkat Lunak (RPL) <br> Tahun 2023/2024</h4>
+    <hr>
+
+    <div class="row mt-3">
+        <div class="col-md-8">
+            <div class="card mb-3">
+                <div class="card-body">
+                    INFORMASI <br>
+                    <hr class="bg-dark">
+                    Aplikai Pengaduan Masyarakat Ini dibuat untuk Memenuhi Tugas Uji Kompetensi Keahlian RPL Tahun 2023 <br><br>
+                    Petunjuk Penggunaan Aplikasi : <br>
+                    <ol>
+                        <li>Registrasi atau buat akun terlebih dahulu</li>
+                        <li>Login menggunakan akun yg telah di registrasi atau di buat (level = masyarakat)</li>
+                        <li>Silahkan ajukan aduan yang ingin di adukan</li>
+                        <li>Monitoring status dari pengaduan tersebut sampai status = selesai</li>
+                        <li>Logout..</li>
+                    </ol>
+                </div>
+                <div class="card-footer"></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    KONTAK <br>
+                    <hr class="bg-dark">
+                    Dikembangkan oleh <br>
+                    NAMA : Ahmad Soleh <br>
+                    KELAS : 12-RPL-3 <br>
+                    NO.HP : 083807173538
+                </div>
+                <div class="card-footer"></div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<footer class="footer mt-3 py-2 bg-primary opacity-40">
+    <div class="container">
+        <p class="text-center" style="color:white;"> UKK RPL 2023 | Ahmad Soleh | SMKN 1 Panyingkiran</p>
+    </div>
+</footer>
+
+<script src="assets/js/bootstrap.min.js"></script>
